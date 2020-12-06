571edfa3873150f766dcebddf577dc86149b6046
Koran Agan <koranagan@gmail.com>
2020-12-06 14:53:50 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,28 @@
 import React from "react"
 import './Footer.css'
 import { ReactComponent as Instagram } from '../assets/icons/instagram.svg'
+import logo from '../assets/aic-logo.gif'
 
 
 function Footer() {
   return (
     <footer>
       <div className='left section'>
-      follow on Instagram<a href='https://www.instagram.com/aic.photography.collection'>
-                  <Instagram className='instagram' /></a><br/>
-           <a href="https://sales.artic.edu/memberships">Art Institute of Chicago - Become a member today.</a><br/>
+      <a href='https://www.instagram.com/aic.photography.collection'>follow on Instagram
+              {/* <Instagram className='instagram' /> */}
+           </a><br />
            <a href="https://www.artic.edu/">AIC public API</a><br />
-           site by: &nbsp;<a href="https://formversuscontent.com">FormVersusContent</a>©2020
-      </div>
-      <div className='right section'>
-        {/* <a href="https://www.artic.edu/">Art Institute of Chicago</a> */}
-        {/* public API<br></br> */}
+           <a href="https://sales.artic.edu/memberships">AIC - Become a member today.</a>
+           {/* <img src={logo} /> */}
+           <br />
+           site by:
            {/* <a href="https://formversuscontent.com">FormVersusContent</a>©2020 */}
+      </div>
+        <div className='right section'>
+           <div>
+               <a href="https://formversuscontent.com" style={{ textTransform: 'none', color: 'rgb(238, 89, 89)' }}>&nbsp;FormVersusContent</a>©2020
+           </div>
+        {/* <img src={logo} /> */}
       </div>
     </footer>
   )
